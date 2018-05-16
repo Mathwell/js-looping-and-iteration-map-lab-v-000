7465ac918d39e2069d73d169a0902ea27f97772d
Olena Ageyeva <eageeva@hotmail.com>
2018-05-16 12:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function lowerCaseDrivers(drivers){
   return drivers.map(function(driver){return driver.toLowerCase()})
 }
+
+function nameToAttributes(drivers){
+  return drivers.map(function(driver){return Object.assign({},{firstName:${driver.split(' ')[0]} }, {lastName:${driver.split(' ')[1] })})
+}
+
+function attributesToPhrase(){
+  
+}
